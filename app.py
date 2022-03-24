66a34501704ef7cdb38eecd49a8c9049ecb9e064
calebthewood <calebwood.cs@gmail.com>
2022-03-24 16:00:15 -0700

    adds sessions and question route

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-RESPONSES = []
-question_id = 0
-
 @app.get("/")
 def get_start():
-    '''Renders Survey Start page'''
+    """Renders Survey Start page"""
 
-    return render_template("survey_start.html",title=survey.title,survey_instructions = survey.instructions)
+    return render_template(
+        "survey_start.html",
+        title=survey.title,
+        survey_instructions=survey.instructions)
 
 @app.post("/begin")
 def start_survey():
@@ -23,17 +23,34 @@ def start_survey():
     return redirect("/questions/0")
 
 @app.get("/questions/<int:question_id>")
-def post_questions(question_id):
+def get_questions(question_id):
     """ show the question page """
-    RESPONSES.append(request.args)
-    print (RESPONSES)
+
     question = survey.questions[question_id]
     choices=survey.questions[question_id].choices
-    return render_template("question.html",choices=choices ,question=question)
-    
-@app.post("/answer")
-def redirect_nextquestion():
-    question_id += 1
-    return redirect(f"/questions/{question_id}")
+
+    return render_template(
+        "question.html",
+        choices=choices,
+        question=question,
+        id=question_id)
+
+@app.post("/questions/<int:question_id>")
+def redirect_nextquestion(question_id):
+
+    session[f"question{q_id}"] = request.form.answer
+    q_id = question_id + 1
+
+    if q_id < len(survey.questions):
+        return redirect(f"/questions/{q_id}")
+    else:
+        return redirect("/completion")
+
+@app.get("/completion")
+def get_completion():
+    """""Renders Completion page after survey is completed"""""
+
+
+    return render_template("completion.html")
 
 
