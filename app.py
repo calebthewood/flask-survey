daa2198a1734d91dadf3c692bc241619c5c36313
lyne cha <lynecha@berkeley.edu>
2022-03-24 12:26:17 -0700

    worked up to question 0 page

--- app.py
+++ app.py
@@ -9,9 +9,31 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 RESPONSES = []
+question_id = 0
 
 @app.get("/")
 def get_start():
     '''Renders Survey Start page'''
 
-    return render_template("survey_start.html",title=survey.title)
+    return render_template("survey_start.html",title=survey.title,survey_instructions = survey.instructions)
+
+@app.post("/begin")
+def start_survey():
+    """ redirects you to the questions once button is clicked """
+    return redirect("/questions/0")
+
+@app.get("/questions/<int:question_id>")
+def post_questions(question_id):
+    """ show the question page """
+    RESPONSES.append(request.args)
+    print (RESPONSES)
+    question = survey.questions[question_id]
+    choices=survey.questions[question_id].choices
+    return render_template("question.html",choices=choices ,question=question)
+    
+@app.post("/answer")
+def redirect_nextquestion():
+    question_id += 1
+    return redirect(f"/questions/{question_id}")
+
+
